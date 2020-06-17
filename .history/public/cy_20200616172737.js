8bb60510b762f2216167869cbbb7ff175196d4a2
Elfebleu9 <poetic.jm@orange.fr>
2020-06-17 17:26:17 +0200

    amélior

---
+++ .history/public/cy_20200616172737.js
@@ -0,0 +1,16 @@
+<script>
+window.onscroll = function() {
+                        let scroll = (document.documentElement.scrollTop ||
+                                document.body.scrollTop);
+        //alert(scroll);
+                        if (scroll > 170) {
+                            document.getElementById('tete').style.position = 'fixed';
+                            document.getElementById('tete').style.top =  '0px';
+                        } else if (scroll < 170 || scroll === 170) {
+                            document.getElementById('tete').style.position = 'absolute';
+                            document.getElementById('tete').style.top = 170 + 'px';
+                        }
+                   
+                }
+
+</script>
---
+++ public/cy.js
@@ -0,0 +1,19 @@
+<script>
+window.onscroll = function() {
+                        let scroll = (document.documentElement.scrollTop ||
+                                document.body.scrollTop);
+        //alert(scroll);
+                        if (scroll > 250) {
+                            document.getElementById('tete').style.position = 'fixed';
+                            
+                            document.getElementById('tete').style.top =  '0px';
+                        } 
+                        else if (scroll < 250 || scroll === 250) {
+                            document.getElementById('tete').style.position = 'absolute' ;
+                           
+                            document.getElementById('tete').style.top = 250 + 'px';
+                        }
+                   
+                }
+
+</script>
